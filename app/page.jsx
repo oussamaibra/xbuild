a0675e0e5683b17ff479a4129210a74be0964ddb
oussamaibra <oussamabr58@gmail.com>
2025-04-11 22:57:27 +0100

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Home1 from "./(homes)/home-1-one-page/page";
+export const metadata = {
+  title: "Home 1 || Xbuild - Constriction nextjs Template",
+  description: "Xbuild - Constriction nextjs Template",
+};
+export default function Home() {
+  return (
+    <>
+      <Home1/>
+    </>
+  );
+}
